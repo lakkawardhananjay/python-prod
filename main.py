85033e366f0832ac07699bf140df65ddf41a456d
lakkawardhananajay <lakkawardhananjay@gmail.com>
2025-07-05 22:56:57 +0530

    new work

---
+++ main.py
@@ -0,0 +1,14 @@
+"""Main module for demonstration."""
+
+
+def main():
+    """Demonstrate basic print statements."""
+    print("syntax error")
+    hey = "hello"
+    print(hey + "hello")
+    onk = hey
+    print(onk)
+
+
+if __name__ == "__main__":
+    main()
